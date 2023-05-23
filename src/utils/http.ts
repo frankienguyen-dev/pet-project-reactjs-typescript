3bfcf8e5cff9e1da8c04491c71bcb14a43a4ac88
frankienguyen-dev <binguyen221198@gmail.com>
2023-05-23 17:31:32 +0700

    Handle error message with axios interceptor

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,4 +1,6 @@
-import axios, { type AxiosInstance } from 'axios';
+import axios, { AxiosError, type AxiosInstance } from 'axios';
+import { toast } from 'react-toastify';
+import HttpStatusCode from 'src/constants/httpStatusCode.enum';
 
 class Http {
   instance: AxiosInstance;
@@ -10,6 +12,36 @@ class Http {
         'Content-Type': 'application/json'
       }
     });
+
+    // Add a request interceptor
+    this.instance.interceptors.request.use(
+      function (config) {
+        // Do something before request is sent
+        return config;
+      },
+      function (error) {
+        // Do something with request error
+        return Promise.reject(error);
+      }
+    );
+
+    // Add a response interceptor
+    this.instance.interceptors.response.use(
+      function (response) {
+        return response;
+      },
+      function (error: AxiosError) {
+        if (error.response?.status !== HttpStatusCode.UnprocessableEntity) {
+          const data: any | undefined = error.response?.data;
+          const message = data.message || error.message;
+          toast.error(message, {
+            autoClose: 1000
+          });
+        }
+
+        return Promise.reject(error);
+      }
+    );
   }
 }
 
